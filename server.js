5fd6d27b813e2ab079d1459cf995a3f337d039e1
Aida <aidapirah@gmail.com>
2024-11-25 19:02:30 -0800

    Fix CORS

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors");
 const mongoose = require('mongoose');
 
 const app = express();
+
+// Add CORS middleware
+app.use(cors({
+  origin: "http://localhost:3000", // Allow requests from this origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+  credentials: true, // Enable cookies and HTTP credentials
+}));
 
 // Middleware
 app.use(express.json());
